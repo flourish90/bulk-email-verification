7fd9a79863075256b0356e1ee8093e5b60990326
flourish90 <flo.schlueter90@gmail.com>
2021-03-20 12:24:54 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,61 @@
+var data;
+var baseElem = document.getElementById("main-table");
+var apiKey;
+
+function parse() {
+    var file = document.getElementById('myDOMElementId').files[0];
+    apiKey = document.getElementById('reacher-api-key').value;
+
+    Papa.parse(file, {
+      header: false,
+      dynamicTyping: true,
+      complete: function(results) {
+        console.log("Finished:" + typeof(results.data));
+        data = results.data;
+        stepthroughResultsAndCallApi(data);
+      }
+    });
+}
+
+function stepthroughResultsAndCallApi(data) {
+  if (!data.error) {
+    for (let index of data) {
+      var node = document.createElement("div");
+      var textnode = document.createTextNode(index[0]);
+      node.appendChild(textnode);
+      node.id = index[0];
+      document.getElementById("main-table").appendChild(node);
+
+      postData('https://ssfy.sh/amaurymartiny/reacher@0d919999/check_email', { "to_email": index[0] })
+        .then(data => {
+          console.log(data);
+          var content = document.getElementById(data.input).innerHTML;
+          document.getElementById(data.input).innerHTML = content + " ### Result: " + data.is_reachable;
+        });
+    }
+  }
+  else {
+    document.getElementById("main-table").innerHTML = data.error;
+  }
+}
+
+async function postData(url, data) {
+  // Default options are marked with *
+  const response = await fetch(url, {
+    method: 'POST', 
+    mode: 'cors', 
+    cache: 'no-cache',
+    credentials: 'same-origin', 
+    headers: {
+      'Content-Type': 'application/json',
+      'authorization': apiKey
+      
+    },
+    redirect: 'follow', 
+    referrerPolicy: 'no-referrer', 
+    body: JSON.stringify(data) 
+  });
+  return response.json();
+}
+
+
